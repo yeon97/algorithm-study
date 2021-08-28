53ac5d419f0d6ec25250811e9b9405da2f64e23b
Nayeon <Nayeon@Nayeon-PS42-Win>
2021-08-28 10:05:31 +0900

    BOJ_10163_색종이

---
+++ 오나연/Main_BOJ_10163_색종이.java
@@ -0,0 +1,42 @@
+package im;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BOJ_10163_색종이 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int cnt = Integer.parseInt(br.readLine());
+		int[][] map = new int[101][101];
+		int[] colorCnt = new int[cnt+1];
+		
+		for(int i=1; i<=cnt; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			int width = Integer.parseInt(st.nextToken());
+			int height= Integer.parseInt(st.nextToken());
+			for(int j=x; j<x+width; j++) {
+				for(int k=y; k<y+height; k++) {
+					map[j][k] = i;
+				}
+			}
+		}
+		
+		for(int j=0; j<101; j++) {
+			for(int k=0; k<101; k++) {
+				if(map[j][k] != 0) {
+					colorCnt[map[j][k]]++;
+				}
+			}
+		}
+		
+		for(int i=1; i<=cnt; i++) {
+			System.out.println(colorCnt[i]);
+		}
+	}
+}
