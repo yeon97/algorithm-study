aff127f123e2f78c4808817f0b4447e9f3672d08
Nayeon <Nayeon@Nayeon-PS42-Win>
2021-08-30 00:19:46 +0900

    BOJ_2798_블랙잭

--- 오나연/Main_BOJ_2798_블랙잭.java
+++ 오나연/Main_BOJ_2798_블랙잭.java
@@ -3,7 +3,6 @@ package im;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_BOJ_2798_블랙잭 {
